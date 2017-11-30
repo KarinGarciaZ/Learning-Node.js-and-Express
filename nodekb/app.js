9837195c170ee2cd4babf57e2a24430bf78038ee
KarinGarciaZ <oscarin962011@hotmail.com>
2017-11-30 08:28:41 -0600

    Now the API saves in the database.

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -25,6 +26,10 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 
+//parse aplication and json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //home route
 app.get('/', (req, res) => {
   Article.find({}, (err, articles) => {
@@ -42,6 +47,19 @@ app.get('/', (req, res) => {
 app.get('/article/add', (req, res) => {
   res.render('add', {
     title: 'AddArticl'
+  });
+});
+
+//Add submit Post route
+app.post('/article/add', (req, res) => {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save( err => {
+    if(err) console.log('err: ', err);
+    else res.redirect('/');
   });
 });
 
